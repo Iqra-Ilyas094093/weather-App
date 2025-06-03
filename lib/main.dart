e55b8595f5772af0b5e557db04f23ad6285e5e9b
Iqra-Ilyas094093 <workflow094093@gmail.com>
2025-06-03 19:36:52 +0500

    start from another video

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:weather_app/utilities/appTheme/themeData.dart';
 import 'package:weather_app/views/mainScreen/mainScreen.dart';
+import 'package:weather_app/views/weatherScreen/weatherScreen.dart';
 import 'package:weather_app/weatherWorld.dart';
 
 void main() {
@@ -19,7 +22,10 @@ class MyApp extends StatelessWidget {
       themeMode: ThemeMode.system,
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
-      home: MainScreen(),
+      home: AnimatedSplashScreen(splash: SizedBox(
+          height: 300,
+          width: 300,
+          child: Lottie.asset('assets/animations/windAnimation.json',)), nextScreen: weatherScreen(),splashIconSize: 1900,),
     );
   }
 }
---
+++ lib/views/weatherScreen/weatherScreen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class weatherScreen extends StatelessWidget {
+  const weatherScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
